b4044a767114f52822843418321be0b29da0a27b
Vladon305 <shuhow003@mail.ru>
2022-07-14 11:07:59 +0300

    remade userProfile component and add logout func

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,15 +1,11 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
-// import { AiOutlineLogout } from 'react-icons/ai'
-import { useParams } from 'react-router-dom'
-// import { googleLogout } from '@react-oauth/google'
+import React, { Dispatch, SetStateAction } from 'react'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom'
+import { googleLogout } from '@react-oauth/google'
 
 import MasonryLayout from '../MasonryLayout'
 import Spinner from '../Spinner'
-import { useTypedSelector } from '../../hooks/useTypedSelector'
-import { useTypedDispatch } from '../../hooks/useTypedDispatch'
-import { getUserProfile } from '../../store/user/userSlice'
-import { getCreatedPins, getSavedPins } from '../../store/pins/reducers'
-import { PinType } from '../../types/types'
+import { PinType, User } from '../../types/types'
 
 const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none'
 const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none'
@@ -19,44 +15,25 @@ const randomImage = "https://source.unsplash.com/1600x900/?nature,photography,te
 type PropsType = {
   createdPins: PinType[]
   savedPins: PinType[]
-  pins: PinType[]
-  setPins: Dispatch<SetStateAction<PinType[]>>
+  id?: string
+  userId: string
+  text: string | null
+  setText: Dispatch<SetStateAction<string | null>>
+  activeBtn: string
+  setActiveBtn: Dispatch<SetStateAction<string>>
+  userProfile: User
 }
 
-const UserProfile: React.FC<PropsType> = ({ createdPins, savedPins, pins, setPins }) => {
-  const [text, setText] = useState<string | null>('Created')
-  const [activeBtn, setActiveBtn] = useState('Created')
+const UserProfile: React.FC<PropsType> = ({ userProfile, createdPins, savedPins,
+  id, userId, text, setText, activeBtn, setActiveBtn }) => {
 
-  const { userProfile } = useTypedSelector(state => state.user)
+  const navigate = useNavigate()
 
-  const dispatch = useTypedDispatch()
-
-  const { userId } = useParams()
-
-  if (!userId) {
-    throw new Error('Unexpected undefined pin id')
+  const logout = () => {
+    googleLogout()
+    localStorage.clear()
+    navigate('/login')
   }
-
-  useEffect(() => {
-    dispatch(getUserProfile(userId)).then(() => {
-      setActiveBtn('Created')
-      setText('Created')
-    })
-    // eslint-disable-next-line
-  }, [userId])
-
-  useEffect(() => {
-    if (text === 'Created') {
-      dispatch(getCreatedPins(userId)).then(() => {
-        setPins(createdPins)
-      })
-    } else {
-      dispatch(getSavedPins(userId)).then(() => {
-        setPins(savedPins)
-      })
-    }
-    // eslint-disable-next-line
-  }, [text, userId])
 
   if (!userProfile) return <Spinner message="Loading profile" />
 
@@ -79,25 +56,17 @@ const UserProfile: React.FC<PropsType> = ({ createdPins, savedPins, pins, setPin
           <h1 className="font-bold text-3xl text-center mt-3">
             {userProfile.userName}
           </h1>
-          {/* <div className="absolute top-0 z-1 right-0 p-2">
-            {userId === user._id && (
-              <GoogleLogout
-                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-                render={(renderProps) => (
-                  <button
-                    type="button"
-                    className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    <AiOutlineLogout color="red" fontSize={21} />
-                  </button>
-                )}
-                onLogoutSuccess={logout}
-                cookiePolicy="single_host_origin"
-              />
+          <div className="absolute top-0 z-1 right-0 p-2">
+            {userId === id && (
+              <button
+                type="button"
+                className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                onClick={logout}
+              >
+                <AiOutlineLogout color="red" fontSize={21} />
+              </button>
             )}
-          </div> */}
+          </div>
         </div>
         <div className="text-center mb-7">
           <button
@@ -121,7 +90,7 @@ const UserProfile: React.FC<PropsType> = ({ createdPins, savedPins, pins, setPin
             Saved
           </button>
         </div>
-        {pins?.length ? (
+        {(text === 'Created' && createdPins.length) || (text === 'Saved' && savedPins.length) ? (
           <div className="px-2">
             <MasonryLayout pins={text === 'Created' ? createdPins : savedPins} />
           </div>
--- src/components/UserProfile/UserProfileContainer.tsx
+++ src/components/UserProfile/UserProfileContainer.tsx
@@ -1,13 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useTypedDispatch } from '../../hooks/useTypedDispatch'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
-import { PinType } from '../../types/types'
+import { getCreatedPins, getSavedPins } from '../../store/pins/reducers'
+import { getUserProfile } from '../../store/user/userSlice'
 import UserProfile from './UserProfile'
 
 const UserProfileContainer: React.FC = () => {
+  const { userProfile } = useTypedSelector(state => state.user)
+  const { _id } = useTypedSelector(state => state.user.user)
   const { createdPins, savedPins } = useTypedSelector(state => state.pins)
-  const [pins, setPins] = useState(null as unknown as PinType[])
+  const [text, setText] = useState<string | null>('Created')
+  const [activeBtn, setActiveBtn] = useState('Created')
 
-  return <UserProfile createdPins={createdPins} savedPins={savedPins} pins={pins} setPins={setPins} />
+  const { userId } = useParams()
+  const dispatch = useTypedDispatch()
+
+  if (!userId) {
+    throw new Error('Unexpected undefined pin id')
+  }
+
+  useEffect(() => {
+    dispatch(getCreatedPins(userId))
+    dispatch(getSavedPins(userId))
+    dispatch(getUserProfile(userId)).then(() => {
+      setActiveBtn('Created')
+      setText('Created')
+    })
+    // eslint-disable-next-line
+  }, [userId])
+
+  useEffect(() => {
+    if (text === 'Created') {
+      dispatch(getCreatedPins(userId))
+    } else {
+      dispatch(getSavedPins(userId))
+    }
+    // eslint-disable-next-line
+  }, [text, userId])
+
+
+  return <UserProfile
+    createdPins={createdPins}
+    savedPins={savedPins}
+    id={_id}
+    activeBtn={activeBtn}
+    setActiveBtn={setActiveBtn}
+    userId={userId}
+    text={text}
+    setText={setText}
+    userProfile={userProfile}
+  />
 }
 
 export default UserProfileContainer
